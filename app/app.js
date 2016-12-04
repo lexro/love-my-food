4da28909c697b744c4a761a0b69af363f76187c1
Lex Lacson <lex.lacson.dev@gmail.com>
2016-12-03 21:33:54 -0800

    link to review from search

--- app/app.js
+++ app/app.js
@@ -10,7 +10,17 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+
+  engines: {
+    search: {
+      dependencies: {
+        externalRoutes: {
+          reviews: 'reviews'
+        }
+      }
+    }
+  }
 });
 
 loadInitializers(App, config.modulePrefix);
